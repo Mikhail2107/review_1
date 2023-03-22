fd4b3bf63562c5705ebdd71e72baf705f705be3c
Mikhail2107 <mixa.proshin@yandex.ru>
2023-03-22 21:04:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,15 +11,14 @@ var slides = document.querySelectorAll('.swiper');
 var sliderSlides = document.querySelector('.slider')
 var buttonShow = document.querySelector('.button--hidden');
 
-buttonShow.addEventListener('click', function(event){    
+buttonShow.addEventListener('click', function(event){
+  buttonShow.classList.toggle('button--visible');
+  sliderSlides.classList.toggle('slider--show');
+  buttonShow.classList.toggle('button--hidden');
   if (event.target.classList.contains('button--visible')){
-    event.target.textContent ='Показать всё';
-    event.target.classList.remove('button--visible');
-    sliderSlides.classList.remove('slider--show');
-}
-else {    
-    event.target.textContent ='Скрыть всё';
-    event.target.classList.add('button--visible');
-    sliderSlides.classList.add('slider--show');        
-} 
+    buttonShow.textContent= 'Скрыть всё';
+    }
+  else {
+    buttonShow.textContent= 'Показать всё';
+    }
 });
